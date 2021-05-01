b3aa3e54ebbdc7217e98a51bf44ec716a0d0baa2
pras75299 <singh.prashantking@gmail.com>
2021-05-02 01:25:03 +0530

    add form, paragraph to show the generated text

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import React, { useState } from 'react';
 import data from './data';
 function App() {
+  const[count, setCount] = useState(0);
+  const[text, setText] = useState([]);
   
-  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    let numberCount = parseInt(count);
+
+    setText(data.slice(0, numberCount));
+  }
+
   return (
-  <h2>lorem ipsum project setup</h2>
+    <section>
+      <h2>Random Text Generator</h2>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="generateCount"></label>
+        <input type="number" id="generateCount" value={count} onChange={(e) => setCount(e.target.value)} />
+        <button type="submit">Generate</button>
+      </form>
+      <article>
+        {text.map((item, index) => {
+          return <p key={index}>{item}</p>
+        })}
+      </article>
+    </section>
     )
 }
 
